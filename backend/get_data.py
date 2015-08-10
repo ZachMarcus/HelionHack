29ffcf28eaf5921f5111395e6e8cdf9914698ba5
spencerhance <spencerhance@gmail.com>
2015-08-10 12:08:22 -0400

    Update get_data

--- backend/get_data.py
+++ backend/get_data.py
@@ -2,12 +2,33 @@
 Database"""
 
 import requests
+import MySQLdb
 
 x_app_token = "WufOy5JwfKNPI1xmcQrK51bUb"
-building_permits_url = "https://data.cityofboston.gov/resource/hfgw-p5wb.json"
 
-r = requests.get(building_permits_url, 
+url_building_permits = "https://data.cityofboston.gov/resource/hfgw-p5wb.json"
+
+request_building_permits = requests.get(url_building_permits, 
         headers={"X-App-Token":x_app_token})
 
-r.json()[0]
+print(request_building_permits.json()[0])
+
+conn = MySQLdb.connect(host="localhost", 
+                    user="test", 
+                    passwd="test", 
+                    db="hphackday")
+
+cur = conn.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS '\
+	'BUILDING_PERMITS(zip, description, city, state, status, '\
+        'location, coordinates, owner, issued_date, applicant, '\
+        'property_id, address, sq_feet, comments, parcel_id, '\
+        'permit_number, occupancytype, permittypedesc, '\
+        'expiration_date, declared_valuation, total_fees, worktype)')
+
+#cur.execute('
+
+
+
 
