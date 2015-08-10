94c407cf2eaf125734eb04f47f7d0ba0d1d81e05
spencerhance <spencerhance@gmail.com>
2015-08-10 16:32:35 -0400

    Updated get_data to be used temporarily in the frontend

--- backend/get_data.py
+++ backend/get_data.py
@@ -5,9 +5,9 @@ import requests
 import mysql.connector
 import os
 import urllib.parse
-from scipy.cluster.vq import vq, kmeans, whiten
+from scipy.cluster.vq import kmeans2, whiten
 
-
+"""
 ##################  DB INFO
 DATABASE_URL = os.getenv('DATABASE_URL')
 db_info = {}
@@ -24,7 +24,7 @@ db_user = db_info['default']['USER']
 db_password = db_info['default']['PASSWORD']
 db_port = db_info['default']['PORT']
 #################### DB INFO
-
+"""
 
 x_app_token = "WufOy5JwfKNPI1xmcQrK51bUb"
 
@@ -33,28 +33,29 @@ url_building_permits = "https://data.cityofboston.gov/resource/hfgw-p5wb.json"
 request_building_permits = requests.get(url_building_permits, 
         headers={"X-App-Token":x_app_token})
 
-conn = mysql.connector.connect(user=db_user,
+"""conn = mysql.connector.connect(user=db_user,
                                password=db_password,
                                host=db_host,
-                               database=db_name)
+                               database=db_name)"""
 
-cur = conn.cursor()
-
+"""cur = conn.cursor()"""
+"""
 # Create building permit table
 cur.execute('CREATE TABLE IF NOT EXISTS '\
-            'BUILDING_PERMITS(centroid, values)')
+            'BUILDING_PERMITS(centroid, values)')"""
 
 ###KMEANS###
 coordinates = []
 for e in request_building_permits.json():
     try:
-        locations.append(e['location']['coordinates'])
+        coordinates.append(e['location']['coordinates'])
     except:
         pass
 centroids, labels = kmeans2(whiten(coordinates), 6, iter = 20)
 norm_labels = [float(i)/max(labels) for i in labels]
 ###KMEANS###
 
+contents = ''
 ###FORMAT DATA FOR OUTPUT###
 for i in range(0, max(labels)):
     output = ''
@@ -63,10 +64,12 @@ for i in range(0, max(labels)):
             new = str(coordinates[j][0]) + ':' + str(coordinates[j][1]) + ':' + \
             str(norm_labels[j]) + ";"
             output += new
+    contents += output
     
-    # Add data to table
-    cur.execute('INSERT OR IGNORE INTO BUILDING_PERMITS '\
-                'VALUES(%d, %s);' % (i, output))
+print(contents)
+"""Add data to table
+cur.execute('INSERT OR IGNORE INTO BUILDING_PERMITS '\
+VALUES(%d, %s);' % (i, output))"""
 
 
 
